516c1c219d90185f71f927af257928bde45a1eb9
Anubhav Saini <IAmAnubhavSaini@gmail.com>
2016-12-06 18:56:05 +0530

    Empty

--- lib/sign.dart
+++
@@ -1,7 +0,0 @@
-class Sign {
-  final String name;
-  final String description;
-  final String caption;
-  final String image;
-  Sign(this.name, this.description, this.caption, this.image);
-}
--- web/main.dart
+++
@@ -1,8 +0,0 @@
-import 'package:angular2/platform/browser.dart';
-// import 'package:traffic_rules/signs.read.component.dart';
-import 'package:traffic_rules/signs.component.dart';
-
-void main() {
-  // bootstrap(SignsReadComponent);
-  bootstrap(SignsComponent);
-}
